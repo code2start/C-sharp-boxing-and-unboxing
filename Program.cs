bdfb6239ffa41aa8ad916e2b2e318ee78a519c27
Mohamed Fathy <codetwostart@gmail.com>
2019-12-13 23:04:45 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingAndUnBoxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 20;//value type
+            Object o1 = i;//Boxing
+            int i1 = (int)o1;//Unboxing
+            Console.WriteLine(i1);
+            Console.WriteLine(string.Concat("Mohamed fathy", 330, 4.5, true));
+            var items = new List<object>();
+            items.Add(10);
+            items.Add("Mohamed");
+            items.Add(true);
+
+            string name = (string)items[1];
+            Console.WriteLine(name);
+        }
+    }
+}
